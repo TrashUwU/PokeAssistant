0eb31318eb04ab4715be3f57829a6a75f85a55cc
Tyler Trucks <68001619+TrashUwU@users.noreply.github.com>
2021-07-18 13:53:27 +0600

    Ruby Test Script

--- test/index.rb
+++ test/index.rb
@@ -3,6 +3,6 @@ def g_21321(_9213, _2123 = 0, _9 = [])
   taken = _2123
   list = _9
   return [taken] if index == list.size
-  get_numbers(list, index+1, taken) +
-  get_numbers(list, index+1, taken + [list[index]])
+  g_21321(list, index+1, taken) +
+  g_21321(list, index+1, taken + [list[index]])
 end
